0a89ff4eac0e381bf22ed6032f528de0dc2923ba
Kartu5514 <106430379+Kartu5514@users.noreply.github.com>
2026-06-18 05:47:23 +0800

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -1,14 +1,23 @@
 function speakText(text){
 
-speechSynthesis.cancel();
+if(!('speechSynthesis' in window)){
+alert("Browser tidak mendukung Text To Speech");
+return;
+}
 
-const utterance =
-new SpeechSynthesisUtterance(text);
+const utterance = new SpeechSynthesisUtterance(text);
 
 utterance.lang = "en-US";
+utterance.rate = 1;
 
-utterance.rate =
-parseFloat(document.getElementById("rate").value);
+utterance.onstart = () => {
+alert("Suara mulai diputar");
+};
+
+utterance.onerror = (event) => {
+alert("Error TTS: " + event.error);
+console.log(event);
+};
 
 speechSynthesis.speak(utterance);
 
