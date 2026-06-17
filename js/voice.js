2891e80a9cb2e2921caa45acad6d625d3ab85d89
Kartu5514 <106430379+Kartu5514@users.noreply.github.com>
2026-06-18 00:34:20 +0800

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -1,10 +1,25 @@
 function speakText(text){
 
+speechSynthesis.cancel();
+
 const utterance =
 new SpeechSynthesisUtterance(text);
 
+utterance.lang = "id-ID";
+
 utterance.rate =
-document.getElementById("rate").value;
+parseFloat(document.getElementById("rate").value);
+
+utterance.volume = 1;
+
+utterance.onstart = () => {
+console.log("Voice started");
+};
+
+utterance.onerror = (e) => {
+console.log("Voice error", e);
+alert("Gagal memutar suara");
+};
 
 speechSynthesis.speak(utterance);
 
