f15a453029a337c5726a3c526ca1c0876eba5407
Kartu5514 <106430379+Kartu5514@users.noreply.github.com>
2026-06-18 05:44:06 +0800

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -5,21 +5,10 @@ speechSynthesis.cancel();
 const utterance =
 new SpeechSynthesisUtterance(text);
 
-utterance.lang = "id-ID";
+utterance.lang = "en-US";
 
 utterance.rate =
 parseFloat(document.getElementById("rate").value);
-
-utterance.volume = 1;
-
-utterance.onstart = () => {
-console.log("Voice started");
-};
-
-utterance.onerror = (e) => {
-console.log("Voice error", e);
-alert("Gagal memutar suara");
-};
 
 speechSynthesis.speak(utterance);
 
