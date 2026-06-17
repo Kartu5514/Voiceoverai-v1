65caef9026ffc3580d55306ebdea78307abf7a94
Kartu5514 <106430379+Kartu5514@users.noreply.github.com>
2026-06-18 00:15:02 +0800

    Update gemini.js

--- js/gemini.js
+++ js/gemini.js
@@ -1,6 +1,7 @@
-const API_KEY = "Tambahkan disini";
-
 async function generateScript(prompt){
+
+const API_KEY =
+document.getElementById("apikey").value;
 
 const response = await fetch(
 `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
@@ -14,8 +15,7 @@ contents:[
 {
 parts:[
 {
-text:
-`Buat script voice over YouTube yang menarik:
+text:`Buat script voice over YouTube yang menarik:
 
 ${prompt}`
 }
