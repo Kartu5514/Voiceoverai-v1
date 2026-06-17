dfe792cd2d6a2e3dfb8caa41a4bb54046e301e7b
Kartu5514 <106430379+Kartu5514@users.noreply.github.com>
2026-06-18 00:01:44 +0800

    Update gemini.js

--- js/gemini.js
+++ js/gemini.js
@@ -3,7 +3,7 @@ const API_KEY = "Tambahkan disini";
 async function generateScript(prompt){
 
 const response = await fetch(
-`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${}`,
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
 {
 method:"POST",
 headers:{
